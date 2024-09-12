6bc367ac5ad0bc58d056445c93711e039147cb17
Dony Lee <122610839+DongSamE@users.noreply.github.com>
2024-09-12 19:58:44 +0900

    Update Ex04_Assign_IncDec_Compare_Logic.java
    Fix typo

--- Ex01_개요/src/Operator/Ex04_Assign_IncDec_Compare_Logic.java
+++ Ex01_개요/src/Operator/Ex04_Assign_IncDec_Compare_Logic.java
@@ -4,7 +4,7 @@ public class Ex04_Assign_IncDec_Compare_Logic {
 
 	public static void main(String[] args) {
 		//	1. Assignment
-		//	Compound assignment operator : +=, -+, *=, /=, %= ,etc
+		//	Compound assignment operator : +=, -=, *=, /=, %= ,etc
 		
 		//	2. Increase/Decrease
 		//	+1(++) or -1(--) to Variable
